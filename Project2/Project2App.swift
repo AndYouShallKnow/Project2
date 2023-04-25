c23cdcfca26c27c80cbab7a2a6420802e45c75f7
gwhite <gerrywhite@gmail.com>
2023-04-25 13:07:18 +0100

    Initial Commit

---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Gerard White on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
