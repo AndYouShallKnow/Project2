3c8ace5ba8e75c08bb66f8aa99dd219f44c5461b
gwhite <gerardwhite@icloud.com>
2023-04-27 11:27:49 +0100

    v1

--- Project2/ContentView.swift
+++ Project2/ContentView.swift
@@ -9,11 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
     
     var body: some View {
         
-        Text("Hi d")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.mint, .black]), startPoint: .top, endPoint: .bottom)
+            
+//            RadialGradient(
+            
+            VStack(spacing: 30){
+                VStack{
+                    Text("Tap the flag of").foregroundColor(.white).font(.subheadline).fontWeight(.heavy)
+                    Text(countries[correctAnswer]).foregroundColor(.white).font(.largeTitle).fontWeight(.bold)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                    .shadow(radius: 5.0)
+                    .clipShape(Capsule())
+                }
+                
+            }
+            
+        }
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
+        }
+    
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func flagTapped(_ number : Int) {
+        
+        if number == correctAnswer {
+              scoreTitle = "Correct"
+            score += 1
+          } else {
+              scoreTitle = "Wrong"
+          }
+
+        showingScore = true
     }
 }
 
@@ -21,6 +73,8 @@ struct ContentView: View {
 func printWarning() -> Void {
     print("Whatever")
 }
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
