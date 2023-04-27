51e21fba681e3db096cc0a314cbb2c8403e3fc1b
gwhite <gerrywhite@gmail.com>
2023-04-27 09:10:13 +0100

    first

--- Project2/ContentView.swift
+++ Project2/ContentView.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Text("Hi")
     }
 }
+
+
+func printWarning() -> Void {
+    print("Whatever")
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ lesson1.swift
@@ -0,0 +1,74 @@
+//
+//  lesson1.swift
+//  Project2
+//
+//  Created by Gerard White on 27/04/2023.
+//
+
+import Foundation
+
+
+//
+//        VStack(alignment: .leading, spacing: 30.0) {
+//            Spacer()
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//            Spacer()
+//            Spacer()
+//        }
+//        .padding()
+
+
+//        HStack{
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+
+
+// Define a blue rectangle
+//            Color.blue
+//            Color.secondary.frame(minWidth: 100, idealWidth: .infinity, maxHeight: 200, alignment: .center)
+//
+/*
+
+ZStack{
+    
+    VStack(spacing: 0){
+        //                Color(.red)
+        //                Color.blue
+        LinearGradient(stops: [
+            Gradient.Stop(color: .white, location: 0.2),
+            Gradient.Stop(color: .mint, location: 0.4)],
+                       startPoint: .top, endPoint: .bottom)
+    }
+    
+    VStack{
+        Text("Gerard's Amazing App")
+            .foregroundStyle(.secondary)
+            .padding(10)
+            .background(.ultraThinMaterial)
+            .font(.largeTitle)
+        
+        Button("Press me!", role: .destructive) {
+            alertShowing = true
+        }
+            .buttonStyle(.bordered)
+            .alert("Important!", isPresented: $alertShowing) {
+                Button("OK", role: .cancel) {}
+            }
+            message:{
+                Text("Please read this.")
+                    }
+    }
+    
+    
+    
+}.ignoresSafeArea()
+
+}
+}
+*/
